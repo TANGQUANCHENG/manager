1bdab1abb8fe99fccfdbca16e5dbffe82519b005
唐全成 <tangqc@ejiaofie.com>
2018-08-31 18:54:32 +0800

    init-version5 图形验证码安装与测试

---
+++ src/main/java/cn/decentchina/manager/common/controller/CommonController.java
@@ -0,0 +1,91 @@
+package cn.decentchina.manager.common.controller;
+
+import cn.decentchina.manager.common.dto.SimpleMessage;
+import cn.decentchina.manager.common.enums.ErrorCodeEnum;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+
+/**
+ * @author 唐全成
+ * @date 2018-08-31
+ */
+@RequestMapping
+@RestController
+public class CommonController {
+    @Autowired
+    private DefaultKaptcha defaultKaptcha;
+
+    /**
+     * 图形验证码
+     *
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @throws Exception
+     */
+    @RequestMapping("/defaultKaptcha")
+    public void defaultKaptcha(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        byte[] captchaChallengeAsJpeg = null;
+        ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
+        try {
+            //生产验证码字符串并保存到session中
+            String createText = defaultKaptcha.createText();
+            httpServletRequest.getSession().setAttribute("verifyCode", createText);
+            //使用生产的验证码字符串返回一个BufferedImage对象并转为byte写入到byte数组中
+            BufferedImage challenge = defaultKaptcha.createImage(createText);
+            ImageIO.write(challenge, "jpg", jpegOutputStream);
+        } catch (IllegalArgumentException e) {
+            httpServletResponse.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        //定义response输出类型为image/jpeg类型，使用response输出流输出图片的byte数组
+        captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
+        httpServletResponse.setHeader("Cache-Control", "no-store");
+        httpServletResponse.setHeader("Pragma", "no-cache");
+        httpServletResponse.setDateHeader("Expires", 0);
+        httpServletResponse.setContentType("image/jpeg");
+        ServletOutputStream responseOutputStream =
+                httpServletResponse.getOutputStream();
+        responseOutputStream.write(captchaChallengeAsJpeg);
+        responseOutputStream.flush();
+        responseOutputStream.close();
+    }
+
+    /**
+     * 图形验证码测试页面
+     *
+     * @return
+     */
+    @RequestMapping("/testKaptcha")
+    public ModelAndView testKaptcha() {
+        return new ModelAndView("testKaptcha");
+    }
+
+    /**
+     * 校验验证码
+     *
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @return
+     */
+    @RequestMapping("/imgVerifyControllerDefaultKaptcha")
+    public SimpleMessage imgVerifyControllerDefaultKaptcha(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        String captchaId = (String) httpServletRequest.getSession().getAttribute("verifyCode");
+        String parameter = httpServletRequest.getParameter("verifyCode");
+        System.out.println("Session  verifyCode " + captchaId +  " form verifyCode " + parameter);
+        if (!captchaId.equals(parameter)) {
+            return new SimpleMessage(ErrorCodeEnum.ERROR, "错误的验证码");
+        } else {
+            return new SimpleMessage(ErrorCodeEnum.OK);
+        }
+    }
+}
