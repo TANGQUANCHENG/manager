0d6f85c275c49a750d641bc02b648c5683693fff
zhongzq <zhongzq@decent.cn>
2018-11-21 11:29:38 +0800

    去掉切面代理注解

--- src/main/java/cn/decentchina/manager/ManagerApplication.java
+++ src/main/java/cn/decentchina/manager/ManagerApplication.java
@@ -2,13 +2,11 @@ package cn.decentchina.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @author 唐全成
  */
 @SpringBootApplication
-@EnableAspectJAutoProxy
 public class ManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerApplication.class, args);
